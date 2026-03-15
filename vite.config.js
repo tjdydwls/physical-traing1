2ee54d3ece1d4f9836fee1f6f0be9936f5bcec2d
tjdydwls123 <tjdydwls123@gmail.com>
2026-03-15 00:31:12 +0000

    Revert base path to root for Cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/physical-traing1/',
+  base: '/',
   server: {
     host: '0.0.0.0',
     port: 3000
